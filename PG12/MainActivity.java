db32e216d9d70eb7f59c05c77d2fb0e54abf5d08
ambilitk99 <95338201+ambilitk99@users.noreply.github.com>
2022-11-01 11:03:47 +0530

    Add files via upload

---
+++ PG12/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.listv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    TextView tv;
+    String items[] = {"LOTUS","ROSE","LILLY","SUNFLOWER"};
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.activity_list_view);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        list.setAdapter(arrayAdapter);
+    }
+}
