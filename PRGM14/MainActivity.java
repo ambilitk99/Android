6965490e3531a6659086fdbb27cac4118f859ad9
ambilitk99 <95338201+ambilitk99@users.noreply.github.com>
2022-11-07 13:12:28 +0530

    Add files via upload

---
+++ PRGM14/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.theme14;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_URL;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_URL=(Button)findViewById(R.id.browse);
+        btn_URL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newintent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com"));
+                startActivity(newintent);
+            }
+        });
+    }
+}
